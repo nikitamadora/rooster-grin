ba527ef33c207bb2fb4d1e93c009597d7b16eb32
nmadora <nikitamadora@gmail.com>
2020-05-18 15:10:50 -0700

    Adds base structure and adds client assets

--- models/Artboard.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const Schema = mongoose.Schema;
-
-export const ArtboardSchema = new Schema({
-  canvasData: String
-})
--- routes/views.js
+++ routes/views.js
@@ -7,22 +7,4 @@ apiRouter.get('/', (req, res) => {
   });
 });
 
-apiRouter.get('/gallery', (req, res) => {
-  res.sendFile('/views/gallery.html', {
-    root: __dirname + '/../'
-  });
-});
-
-apiRouter.get('/about', (req, res) => {
-  res.sendFile('/views/about.html', {
-    root: __dirname + '/../'
-  });
-});
-
-apiRouter.get('/view/:id', (req, res) => {
-  res.sendFile('/views/printView.html', {
-    root: __dirname + '/../'
-  });
-});
-
 export default apiRouter;
