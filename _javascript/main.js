616cce429dc44a802151c2276e9a238592a8fd6c
nmadora <nikitamadora@gmail.com>
2020-05-19 15:04:27 -0700

    Adds click handler function; minor style tweaks

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,4 +1,19 @@
 
+// ----------------------------
+//           Global
+// ----------------------------
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello, Developer!');
 });
+
+// ----------------------------
+//        Event Handlers
+// ----------------------------
+const clickAlert = () => {
+  alert(`You've clicked a button!`);
+};
+
+const heroButton = document.querySelector('#hero-button');
+heroButton.addEventListener('click', clickAlert);
+
+
--- dist/lib/main.js
+++ dist/lib/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
+// ----------------------------
+//           Global
+// ----------------------------
 document.addEventListener('DOMContentLoaded', function () {
   console.log('Hello, Developer!');
 });
+
+// ----------------------------
+//        Event Handlers
+// ----------------------------
+var clickAlert = function clickAlert() {
+  alert('You\'ve clicked a button!');
+};
+
+var heroButton = document.querySelector('#hero-button');
+heroButton.addEventListener('click', clickAlert);
